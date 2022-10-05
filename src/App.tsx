e525b83e34aba5f70d71092b999dae78a3746874
Rikke Paulsen <ripa@Rikkes-MacBook-Pro.local>
2022-10-05 16:55:02 +0200

    deployed version full of bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import React from "react";
-import "./App.css";
-import { Home } from "./pages/home";
-import { Wedding } from "./pages/wedding";
-import { Couple } from "./pages/couple";
-import { TopBanner } from "./components/TopBanner";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { Home } from './pages/home';
+import { Wedding } from './pages/wedding';
+import { Couple } from './pages/couple';
+import { TopBanner } from './components/TopBanner';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import i18next from 'i18next';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <TopBanner />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Routes>
-          <Route path="/wedding" element={<Wedding />} />
-        </Routes>
-        <Routes>
-          <Route path="/couple" element={<Couple />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <TopBanner />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+                <Routes>
+                    <Route path="/wedding" element={<Wedding />} />
+                </Routes>
+                <Routes>
+                    <Route path="/couple" element={<Couple />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -1,66 +1,75 @@
-import Toolbar from "@mui/material/Toolbar";
-import { Paper, Button, Grid, IconButton, styled, Typography } from "@mui/material";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { useState } from "react";
-import {useTranslation} from "react-i18next";
+import Toolbar from '@mui/material/Toolbar';
+import {
+    Paper,
+    Button,
+    Grid,
+    IconButton,
+    styled,
+    Typography
+} from '@mui/material';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const TopBanner = () => {
-  
-  const {t, i18n} = useTranslation('common');
-  const [isEnglish, setEnglish] = useState(false)
-  const handleLanguageChange = () => {
-    if (isEnglish) {
-      i18n.changeLanguage('no');
-      setEnglish(false);
-    } else{
-      i18n.changeLanguage('en');
-      setEnglish(true);
-    }
-  };
-  return (
-    <>
-      <StyledPaperContainer>
-        <StyledHeader>{t("header.title")}</StyledHeader>
-        <StyledDescription>{t("header.date")}</StyledDescription>
-      </StyledPaperContainer>
-      <StickyTopNav>
-        <Toolbar>
-          <IconButton href={"/"}>
-            <FavoriteBorderIcon />
-          </IconButton>
-          <Grid container justifyContent={"end"}>
-            <Button href={"/"}>{t("navBar.home")}</Button>
-            <Button href={"/couple"}>{t("navBar.couple")}</Button>
-            <Button href={"/wedding"}>{t("navBar.wedding")}</Button>
-            <Button href={"/wedding"}>{t("navBar.rsvp")}</Button>
-            <Button onClick={handleLanguageChange}>{isEnglish ? t("navBar.norwegian") : t("navBar.english")}</Button>
-
-          </Grid>
-        </Toolbar>
-      </StickyTopNav>
-    </>
-  );
+    const { t, i18n } = useTranslation('common');
+    const [isEnglish, setEnglish] = useState(false);
+    const handleLanguageChange = () => {
+        if (isEnglish) {
+            i18n.changeLanguage('no');
+            setEnglish(false);
+        } else {
+            i18n.changeLanguage('en');
+            setEnglish(true);
+        }
+    };
+    return (
+        <>
+            <StyledPaperContainer>
+                <StyledHeader>{t('header.title')}</StyledHeader>
+                <StyledDescription>{t('header.date')}</StyledDescription>
+            </StyledPaperContainer>
+            <StickyTopNav>
+                <Toolbar>
+                    <IconButton href={'/'}>
+                        <FavoriteBorderIcon />
+                    </IconButton>
+                    <Grid container justifyContent={'end'}>
+                        <Button href={'/'}>{t('navBar.home')}</Button>
+                        <Button href={'/couple'}>{t('navBar.couple')}</Button>
+                        <Button href={'/wedding'}>{t('navBar.wedding')}</Button>
+                        <Button href={'/wedding'}>{t('navBar.rsvp')}</Button>
+                        <Button onClick={handleLanguageChange}>
+                            {isEnglish
+                                ? t('navBar.norwegian')
+                                : t('navBar.english')}
+                        </Button>
+                    </Grid>
+                </Toolbar>
+            </StickyTopNav>
+        </>
+    );
 };
 
 const StickyTopNav = styled(Grid)`
-  position: sticky;
-  top: 0;
-  background: white;
+    position: sticky;
+    top: 0;
+    background: white;
 `;
 const StyledHeader = styled(Typography)`
-  color: white;
-  font-size: 100px;
-  font-family: Apple Chancery;
+    color: white;
+    font-size: 100px;
+    font-family: Apple Chancery;
 `;
 const StyledDescription = styled(Typography)`
-  color: white;
-  font-size: 50px;
-  font-family: Apple Chancery;
+    color: white;
+    font-size: 50px;
+    font-family: Apple Chancery;
 `;
 
 const StyledPaperContainer = styled(Paper)`
-  background-image: url("/images/image1.jpg");
-  background-size: cover;
-  background-position: center;
-  height: 300px;
+    background-image: url('/images/image1.jpg');
+    background-size: cover;
+    background-position: center;
+    height: 300px;
 `;
